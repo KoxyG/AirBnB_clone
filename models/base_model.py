f70e851018726762fe7e61951d898b6573d7e59f
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-11-23 04:30:17 -0800

    just started

---
+++ models/base_model.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""""This is a script in the base model"""
+
+class BaseModel:
+    """"This is a class from which other classes will inherit from"""
+    
+    def __init__(self):
+        """initializing instance attributes"""
