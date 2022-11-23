6a0c3ed1b43cfa925245a825b2e20302b0114948
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-11-23 05:50:49 -0800

    still working

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 """"This is a script in the base model"""
 
+import uuid
+from datetime import datetime 
+
 class BaseModel:
     """"This is a class from which other classes will inherit from"""
     
     def __init__(self):
         """initializing instance attributes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+
+    def __str__(self):
+        """defines a format for the string representation of the class"""
+        return "[{}] ({}) {}".\format(type(self).
