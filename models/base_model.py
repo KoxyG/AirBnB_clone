2e0c2976dfa665dd954ce67a2c1b0048a80594a2
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-11-23 15:53:25 -0800

    done

--- models/base_model.py
+++ models/base_model.py
@@ -2,16 +2,36 @@
 """"This is a script in the base model"""
 
 import uuid
-from datetime import datetime 
+from datetime import datetime
+
+
 
 class BaseModel:
+
     """"This is a class from which other classes will inherit from"""
-    
-    def __init__(self):
-        """initializing instance attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+    def __init__(self, *args, **kwargs):
+        """initializing instance attributes
+
+        Args:
+           - *args: list of arguments
+           - **kwargs: dict of key-value arguments
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
 
     def __str__(self):
@@ -26,6 +46,8 @@ class BaseModel:
     def to_dict(self):
        """returns a dictionary containing all keys/values of the instance"""
        my_dict = self.__dict__.copy()
-       my_dict["__class__"] = type(self).__name__
+       my_dict["__class__"] = type(self).__name_
+       my_dict["created_at"] = my_dict["created_at"].isoformat()
+       my_dict["updated_at"] = my_dict["updated_at"].isoformat()
 
        return my_dict 
