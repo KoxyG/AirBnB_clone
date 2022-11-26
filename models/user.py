03fe5101957c8427ae95357aa7473c6a06dd7fc9
Tryph-ena <stesha534@gmail.com>
2022-11-25 18:18:42 -0800

    working

---
+++ models/user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from models.base_model import base model
+
+class User(BaseModel):
+    """A public class attribute that inherits from Basemodel"""
+    email = ""
+    password = ""
+    firts_name = ""
+    last_name = ""
